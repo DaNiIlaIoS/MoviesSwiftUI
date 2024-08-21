bb280133fac9785a32e0dccd7f8ffde59131798e
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-08-21 18:01:52 +0500

    Initial Commit

---
+++ MoviesSwiftUI/MoviesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesSwiftUIApp.swift
+//  MoviesSwiftUI
+//
+//  Created by Даниил Сивожелезов on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
